a13f1349fb3dd29a9e6b74f8b4287f3a3c48f83f
Joseph Hartline <mikey83mikey83@gmail.com>
2026-05-03 13:17:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Request
+import square.client
+import shippo
 import os
 
 app = FastAPI()
 
-# This links your 'src' folder so CSS/JS files can be found
-# Make sure your folder is actually named 'src' in GitHub
-app.mount("/src", StaticFiles(directory="src"), name="src")
+# 1. Connect to Square (Using the Secrets you put in GitHub)
+square_client = square.client.Client(
+    access_token=os.environ.get('SQUARE_ACCESS_TOKEN'),
+    environment='production' 
+)
 
-@app.get("/")
-async def read_index():
-    # This sends the index.html file to the browser
-    return FileResponse(os.path.join("src", "index.html"))
+# 2. Connect to Shippo
+shippo.api_key = os.environ.get('SHIPPO_API_KEY')
+
+# --- THE BUTTON LOGIC ---
+
+@app.post("/create-repair-ticket")
+async def create_claim(data: Request):
+    # This powers the 'Warranty Hub' button
+    # It saves the customer info and tells Shippo to email a label
+    form_data = await data.json()
+    
+    # Logic to generate a return label via Shippo
+    shipment = shippo.Shipment.create(
+        address_from={
+            "name": form_data['name'],
+            "street1": form_data['address'],
+            "city": form_data['city'],
+            "state": form_data['state'],
+            "zip": form_data['zip'],
+            "country": "US"
+        },
+        address_to="YOUR_ALLIANCE_OHIO_ADDRESS",
+        parcels=[{"length": "12", "width": "12", "height": "12", "distance_unit": "in", "weight": "10", "mass_unit": "lb"}]
+    )
+    return {"status": "Label Sent", "tracking": shipment.tracking_number}
+
+@app.get("/inventory")
+def get_parts():
+    # This pulls from your Supabase table and shows it on the 'Inventory' button
+    # We'll link this directly to the frontend
+    pass
     
